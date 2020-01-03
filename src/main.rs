af26e829c1faa6571f046e880e12c2a76b9c9aed
Christopher J. McClellan <cmcclellan@pillartechnology.com>
2020-01-02 20:11:50 -0500

    Blink a light

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,32 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
 
-#[allow(unused_imports)] // Need to link the device crate to compile correctly
 use stm32f3xx_hal::prelude::*;
-// use stm32f3xx_hal::stm32;
+use stm32f3xx_hal::stm32;
+use stm32f3xx_hal::delay::Delay;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+
+    let device_periphs = stm32::Peripherals::take().unwrap();
+    let mut reset_control_clock = device_periphs.RCC.constrain();
+    let mut gpioe = device_periphs.GPIOE.split(&mut reset_control_clock.ahb);
+
+    let mut led = gpioe
+        .pe13
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+
+    led.set_low().unwrap();
+
+    let core_periphs = cortex_m::Peripherals::take().unwrap();
+    let mut flash = device_periphs.FLASH.constrain();
+    let clocks = reset_control_clock.cfgr.freeze(&mut flash.acr);
+    let mut delay = Delay::new(core_periphs.SYST, clocks);
 
     loop {
-        // your code goes here
+        led.toggle().unwrap();
+        delay.delay_ms(1000u16);
     }
 }
